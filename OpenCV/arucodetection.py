1823280836dd451e440cf654a24d65cb42a7c7de
ProgFrog <artemalekseenko51@gmail.com>
2024-10-15 15:54:00 +0300

    Ready|Feat: add aruco marker detection

---
+++ OpenCV/arucodetection.py
@@ -0,0 +1,36 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+def generate_marker(marker_id, marker_size, directory, filename):
+    aruco_dict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_6X6_250)
+
+    marker_image = cv2.aruco.generateImageMarker(aruco_dict, marker_id, marker_size)
+
+    cv2.imwrite(f'{directory}/{filename}', marker_image)
+    plt.imshow(marker_image, cmap='gray', interpolation='nearest')
+    plt.axis('off')  # Hide axes
+    plt.title(f'ArUco Marker {marker_id}')
+    plt.show()
+
+def detect_marker():
+    cap = cv2.VideoCapture(0)
+
+    dictionary = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_6X6_250)
+    parameters = cv2.aruco.DetectorParameters()
+    detector = cv2.aruco.ArucoDetector(dictionary, parameters)
+
+    while True:
+        ret, frame = cap.read()
+
+        markerCorners, markerIds, rejectedCandidates = detector.detectMarkers(frame)
+
+        new_frame = cv2.aruco.drawDetectedMarkers(frame, markerCorners, markerIds)
+
+        cv2.imshow('Found markers2', new_frame)
+        if cv2.waitKey(2) & 0xFF == ord('q'):
+            break
+
+
+if __name__ == '__main__':
+	detect_marker()
